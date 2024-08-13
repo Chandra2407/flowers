5ca8668b05987cda0eef9a8c1c793b2960f3b0e6
chandra2407 <chandra240797@gmail.com>
2024-08-13 19:32:22 +0530

    finally finished

--- main.js
+++ main.js
@@ -5,8 +5,17 @@ let pickUpLines = [
   "Are you a rose? Because you make my heart bloom.",
   "Roses are red, violets are blue, I’m not a poet, but I’m falling for you.",
   "i know i was stupid, sorryyy seriously.",
-  "Aur like please dont get mad about these corny pick up lines, mujhe achhe nhi mile. jb milenge tb update kr dunga",
-  "also abhi tujhe same sentence multiple times dikh rha hoga, ise bhi fix kr dunga okay"
+  "also abhi tujhe same sentence multiple times dikh rha hoga, ise bhi fix kr dunga okay",
+  "there are three type of flowers in this garden, can you guess them???",
+  "Are you a daisy? Because I’m crazy about you.",
+  "I can't be-leaf how amazing you are!",
+  "How do sunflowers stay so positive? They always look on the sunny side!",
+  "What do you call a clumsy flower? A Whoopsi-Daisy.",
+  "What do you call a flower that runs on electricity? A power plant!",
+  "Why did the rose refuse to go to the party? It didn't want to get caught in a thorny situation!",
+  "Why did the gardener take a photo of a flower? To help it photosynthesize",
+  "I'm rooting for you, my little sunflower.",
+  "Life would be rosy if we could just bloom wherever we're planted!"
 ]
 
 btn.addEventListener("click",()=>{
