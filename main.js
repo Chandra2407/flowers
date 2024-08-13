c25140c5403fb768db066afe5302af78b328e30e
chandra2407 <chandra240797@gmail.com>
2024-08-13 18:50:34 +0530

    delete flowers functionality

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 const btn = document.getElementById("btn")
+const flowers = document.querySelectorAll(".flower")
+
 btn.addEventListener("click",()=>{
   window.location.reload()
 })
+
+function addPressEventOnFlowers(){
+  flowers.forEach((flower)=>{
+    flower.addEventListener("click",(e)=>{
+        let elem = e.target
+        if(elem.className == "circle"){
+          elem.parentNode.remove()
+        }
+    })
+  })
+}
+
+addPressEventOnFlowers()
